c2a463911d7012fd14b48447f298afa2d510bec1
Felipe Tanios <felipetanios@MacBook-Air-de-Felipe.local>
2018-08-14 21:52:48 -0300

    adicionadas as rotinas da maquina de estados que tratam dos estados de dia do semaforo

--- TrabalhoSemaforo/Semaforo/Semaforo.ino
+++ TrabalhoSemaforo/Semaforo/Semaforo.ino
@@ -15,18 +15,19 @@ int sensor_luz;
 
 //Flags setadas pelas interrupções
 int flag_botao;
-int flag_timer;
 
 int val_sensor_luz;
 unsigned int tempo;
+unsigned int tempo_antes;
 
 void setup() {
 	/*Estados:
 	* 0 - carro: verde, pedestre: vermelho
-	* 1 - carro: piscando amarelo, pedestre: piscando vermelho
-	* 2 - carro: amarelo, pedestre: vermelho
-	* 3 - carro: vermelho, pedestre: verde
-	* 4 - carro: vermelho, pedestre: piscando vermelho
+	* 1 - carro: amarelo, pedestre:  vermelho
+	* 2 - carro: vermelho, pedestre: verde
+	* 3 - carro: vermelho, pedestre: piscando vermelho
+	* 4 - transicao dia e noite
+	* 5 - carro: piscando amarelo, pedestre: piscando vermelho
 	*/
 	estado = 0;
 	
@@ -65,26 +66,32 @@ void setup() {
 }
 
 void loop() {
-
-
-  if (estado == 0) {
-    //se estiver de dia
-    digitalWrite(verde_carros, HIGH);
-    digitalWrite(amarelo_carros, LOW);
-    digitalWrite(vermelho_carros, LOW);
-    digitalWrite(verde_pedestres, LOW);
-    digitalWrite(vermelho_pedestres, HIGH);
-    //interrupções verificando o pulsar do botão, caso for pressionado, chama função pra realizar as mudanças de estado pro pedestre poder passar.
-    //fecha_semaforo();
-
-  }
-  else{
-    //de noite
-    //pisca amarelo pros carros
-    //pisca vermelhor pro pedestre
-
-    //apertar botão não faz efeito
-  }
+	//maquina de estados
+	switch(estado){
+		case 0:
+			semaforo_carro_aberto();
+			break;
+		case 1:
+			semaforo_carro_amarelo();
+			flag_botao = 0;
+			break;
+		case 2:
+			semaforo_carro_vermelho();
+			flag_botao = 0;
+			break;
+		case 3:
+			semaforo_pedestre_piscando();
+			flag_botao = 0;
+			break;
+		case 4:
+			flag_botao = 0;
+			break;
+		case 5:
+			flag_botao = 0;
+			break;
+		default:
+			break;
+	}
 
 }
 
@@ -97,31 +104,87 @@ void contador_tempo (){
 void seta_flag_botao(){
 	unsigned int tempo_aux = tempo;
 	//evita debounce
-	while(tempo_aux + 3 <  tempo);
+	while(abs(tempo - tempo_aux) <  3);
 	if (digitalRead(3) == 1){
 		flag_botao = 1;
 	}
+}
+
+void semaforo_carro_aberto(){
+	digitalWrite(verde_carros, HIGH);
+	digitalWrite(amarelo_carros, LOW);
+	digitalWrite(vermelho_carros, LOW);
+	digitalWrite(verde_pedestres, LOW);
+	digitalWrite(vermelho_pedestres,HIGH);
+	if (flag_botao){
+		flag_botao = 0;
+		tempo_antes = tempo;
+		estado = 1;
+	}
+}
+
+void semaforo_carro_amarelo(){
+	digitalWrite(verde_carros, LOW);
+	digitalWrite(amarelo_carros, HIGH);
+	digitalWrite(vermelho_carros, LOW);
+	digitalWrite(verde_pedestres, LOW);
+	digitalWrite(vermelho_pedestres,HIGH);
+	//se tiver passado 5 segundos
+	if (abs(tempo - tempo_antes) > 500 ){
+		tempo_antes = tempo;
+		estado = 2;
+	}
+}
+
+void semaforo_carro_vermelho(){
+	digitalWrite(verde_carros, LOW);
+	digitalWrite(amarelo_carros, LOW);
+	digitalWrite(vermelho_carros, HIGH);
+	digitalWrite(verde_pedestres, HIGH);
+	digitalWrite(vermelho_pedestres,LOW);
 	
+	//se tiver passado 15 segundos
+	if (abs(tempo - tempo_antes) > 1500 ){
+		tempo_antes = tempo;
+		estado = 3;
+	}
+}
+
+void semaforo_pedestre_piscando(){
+	digitalWrite(verde_carros, LOW);
+	digitalWrite(amarelo_carros, LOW);
+	digitalWrite(vermelho_carros, HIGH);
+	digitalWrite(verde_pedestres, LOW);
+
+	//pisca o vermelho (inverte seu valor logico a cada 0.5s)
+	if (abs(tempo - tempo_antes) > 50){
+		digitalWrite(vermelho_pedestres,!digitalRead(vermelho_pedestres));
+	}
+	//se tiver passado 5 segundos
+	if (abs(tempo - tempo_antes) > 500 ){
+		tempo_antes = tempo;
+		estado = 0;
+	}
 }
 
 
-void fecha_semaforo() {
-  //espera 5 segundos
-  //amarelo pros carros
-  digitalWrite(verde_carros, LOW);
-  digitalWrite(amarelo_carros, HIGH);
-  //mais 5 segundos:
-  //fecha pros carros e abre pro pedestre
-  digitalWrite(amarelo_carros, LOW);
-  digitalWrite(vermelho_carros, HIGH);
-  digitalWrite(verde_pedestres, HIGH);
-  //20 segundos
-  digitalWrite(verde_pedestres, LOW);
-  //pisca o vermelho do pedestre
+// void fecha_semaforo() {
+//   //espera 5 segundos
+//   //amarelo pros carros
+//   digitalWrite(verde_carros, LOW);
+//   digitalWrite(amarelo_carros, HIGH);
+//   //mais 5 segundos:
+//   //fecha pros carros e abre pro pedestre
+//   digitalWrite(amarelo_carros, LOW);
+//   digitalWrite(vermelho_carros, HIGH);
+//   digitalWrite(verde_pedestres, HIGH);
+//   //20 segundos
+//   digitalWrite(verde_pedestres, LOW);
+//   //pisca o vermelho do pedestre
 
-  //5 segundos
-  digitalWrite(vermelho_carros, LOW);
-  digitalWrite(vermelho_pedestres, HIGH);
-  digitalWrite(verde_carros, HIGH);
-}
+//   //5 segundos
+//   digitalWrite(vermelho_carros, LOW);
+//   digitalWrite(vermelho_pedestres, HIGH);
+//   digitalWrite(verde_carros, HIGH);
+// }
 
