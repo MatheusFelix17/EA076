d462ffb53c85477afd5de10b152b1044c156b2a7
Felipe Tanios <felipetanios@eduroam-177-220-34-237.fee.unicamp.br>
2018-09-19 00:37:57 -0300

    projeto finalmente funcionando

--- projeto2/projeto2.ino
+++ projeto2/projeto2.ino
@@ -8,11 +8,13 @@
   Portas utilizadas:
   MOTOR:
 */
-int enable = 7;
+char enable = 9;
 int pinoA = 6;
-int pinoB = 5;
-int velocidade = 0;
+int pinoB = 7;
 int vel = 0;
+int RPM = 0;
+int velocidade_RPM;
+int contador_interrupt;
 /*UART
 */
 int tx = 1;
@@ -25,32 +27,46 @@ int encoder_interrupt = 3;
 /*
  * Instancia a porta
  * 
- * pin 8 - Serial clock out (SCLK)
- * pin 9 - Serial data out (DIN)
- * pin 10 - Data/Command select (D/C)
- * pin 11 - LCD chip select (CS/CE)
- * pin 12 - LCD reset (RST)
+ * pin 13 (SPI) - Serial clock out (SCLK)
+ * pin 11 (SPI)- Serial data out (DIN)
+ * pin 9 - Data/Command select (D/C)
+ * pin 8 - LCD chip select (CS/CE)
+ * pin 4 - LCD reset (RST)
  */
 
-Adafruit_PCD8544 display = Adafruit_PCD8544(8, 9, 10, 11, 12);
+Adafruit_PCD8544 display = Adafruit_PCD8544(5, 8, 4);
 
 /*
  * FLAG VENTILADOR
 */
 int flag_vent = 0;
 
+/*
+ * Contadores de tempo
+ */
+
+unsigned int tempo_interrupt;
+
 void setup()
 {
+  /*
+   * Inicializa o bluetooth
+   */
   Serial.begin(9600);
-  pinMode(10, OUTPUT);
-  Timer1.initialize(500000);         // initialize timer1, and set a 1/2 second period
+
+  /*
+   * Inicializa o timer e a interrupção por tempo
+   */
+  Timer1.initialize(1000);         //inicializa o timer com intervalo de tempo de 1ms
+  Timer1.attachInterrupt(somaTempo);
   /*
    * Inicializa o motor
    */
-  pinMode(pinoA, OUTPUT);
-  pinMode(pinoB, OUTPUT);
-  Timer1.pwm(enable, velocidade);                // setup pwm on pin 9, 50% duty cycle\
-
+  Timer1.pwm(enable, vel);                
+  /**
+   * Inicializa a função de tratamento da interrupçào externa (encoder)
+   */
+   attachInterrupt(digitalPinToInterrupt(3), somaRPM, RISING);
   /*
    * Inicializa o Visor LCD
   */
@@ -62,12 +78,47 @@ void setup()
   display.setCursor(0,0);  //Seta a posição do cursor
   //display.println("Arduino & Cia!");
   display.display();
+
+  /*
+   * inicializa os contadores de tempo
+   */
+   tempo_interrupt = 0;
+   /*
+    * Inicializa o contador de passagens da haste na frente do sensor (calculo de velocidade)
+    */
+    contador_interrupt = 0;
+    velocidade_RPM = 0;
 }
 
 
 
-void loop()
-{
+void loop(){
+  /*
+   * Le a entrada serial enviada via Bluetooth
+   */
+  trata_entrada();
+  calcula_velocidade();
+}
+
+/*
+ * Incrementa o contador de tempo
+ */
+void somaTempo(){
+  tempo_interrupt += 1;
+}
+
+/*
+ * Incrementa o contador de interrupções externas (encoder)
+ */
+void somaRPM(){
+  contador_interrupt += 1;
+}
+
+
+/*
+ * Trata A entrada serial que chega pelo HC-06 (módulo Bluetooth)
+ */
+void trata_entrada(){
   char c;
   String content = "";
   int limite = 8; //tamanho maximo de um comando permitido
@@ -89,9 +140,9 @@ void loop()
       Serial.println("Erro: tamanho maximo do comando excedido!");  //ERRO: COMANDO INEXISTENTE
     }
     else {
-//      Serial.print("tamanho do comando válido, que é: ");
-//      Serial.println(content);
-
+      /**
+       * Checa se o que foi recebido é algum comando valido e executa o comando
+       */
       if (content == "VENT*") {
         Serial.println("OK VENT");
         ventilador();
@@ -108,14 +159,14 @@ void loop()
       }
 
       else if (content == "RETVEL*") {
-        Serial.print("VEL");
-        Serial.print(" X ");  //numero
-        Serial.println("RPM");
-
-        //chama alguma coisa aqui
+        String string_vel_retorno = "";
+        string_vel_retorno += "VEL: ";
+        string_vel_retorno +=velocidade_RPM;
+        string_vel_retorno += " RPM";
+        Serial.println(string_vel_retorno);
       }
 
-      else if (content.substring(0,3) == "VEL") {
+      else if (content.substring(0,4) == "VEL " || content.substring(0,4) == "VEL*" ) {
         
         //vamos verificar se os próximos digitos de VEL
         String resto = content.substring(4,8);
@@ -125,20 +176,11 @@ void loop()
 
         else {
           if (resto[3] == '*') {
-//            Serial.print("Substring: ");
-//            Serial.println(resto);
             if(isDigit(resto[0]) && isDigit(resto[1]) && isDigit(resto[2])) {
-//              Serial.println("Os 3 valores são numeros");
-              vel = resto.toInt();  
-//              Serial.print("Numero convertido = ");
-//              Serial.println(vel);   
-              if (vel >= 0 && vel <= 100) {
-//                Serial.println("Comando da velocidade OK!");    
-                String resposta = "OK VEL ";
-                //resposta.concat(vel);
-                //resposta.concat('%');   
+              vel = resto.toInt();    
+              if (vel >= 0 && vel <= 100) {  
+                String resposta = "OK VEL "; 
                 Serial.println(resposta);
-                //CHAMA A FUNÇÃO
                 seta_speed(vel);
               }
               else
@@ -159,42 +201,67 @@ void loop()
     }
   
   }
-
 }
 
+/*
+ * Calcula a velocidade em RPM
+ */
+void calcula_velocidade(){
+   if (tempo_interrupt >= 100){ //se passou 100ms
+      float velocidade_hz = contador_interrupt * 10.0 / 2;
+      velocidade_RPM = 60*velocidade_hz;
+      //chama a função para escrever na tela
+      seta_speed(vel);
+      contador_interrupt = 0;
+      tempo_interrupt = 0;
+   }
+}
 
+/*
+ * Funcao para ativação do motor no modo ventilador
+ */
 void ventilador()
 {
-   parado();
    flag_vent = 1;
+   parado();
    digitalWrite(pinoA, HIGH);
    digitalWrite(pinoB, LOW);
    print_ventilador();
-   //digitalWrite(enable, HIGH);
 }
 
+/*
+ * Funcao para ativação do motor no modo exaustor
+ */
 void exaustor()
 {
-   parado();
    flag_vent = 0;
+   parado();
    digitalWrite(pinoA, LOW);
    digitalWrite(pinoB, HIGH);
    print_exaustor();
-   //digitalWrite(enable, HIGH);
 }
 
+/*
+ * Função para parar o motor
+ */
 void parado()
 {
-   //digitalWrite(pino, !digitalRead(pino));
-   seta_speed(0);
+   digitalWrite(pinoA, LOW);
+   digitalWrite(pinoB, LOW);
    print_parado();
    delay(200);
 }
 
+/*
+ * Função que define a velocidade do motor baseado no duty cicle do PWM que ativa ele (enable)
+ * O parametro de entrada da função varia de 0 a 100, porém o parametro do PWM é de 0 a 1023, 
+ * então fazemos a transformação dos valores
+ */
 void seta_speed(int vel)
 {
-   velocidade = (int) (vel*1024/100);
-   Timer1.pwm(enable, velocidade);
+  //Serial.println(vel);
+   int velocidade = (int)(vel*(1024.0/100));
+   Timer1.setPwmDuty(enable, velocidade);
 
   if (flag_vent == 1){
     print_ventilador();
@@ -205,38 +272,57 @@ void seta_speed(int vel)
 }
 
 
-
+/**
+ * Funcao para imprimir display LCD o modo ventilador
+ */
 void print_ventilador(){
   String string_velocidade = "";
+  String string_rpm = "";
   display.clearDisplay();   //Apaga o buffer e o display
   display.setCursor(0,0);
   display.println("VENTILADOR");
   string_velocidade += vel;
   string_velocidade += "%";
   display.println(string_velocidade);
+  string_rpm += velocidade_RPM;
+  string_rpm += " RPM";
+  display.println(string_rpm);
   display.display();
 }
 
+/**
+ * Funcao para imprimir display LCD o modo exaustor
+ */
 void print_exaustor(){
   String string_velocidade = "";
+  String string_rpm = "";
   display.clearDisplay();   //Apaga o buffer e o display
   display.setCursor(0,0);
   display.println("EXAUSTOR");
   string_velocidade += vel;
   string_velocidade += "%";
   display.println(string_velocidade);
+  string_rpm += velocidade_RPM;
+  string_rpm += " RPM";
+  display.println(string_rpm);
   display.display();
 }
 
-
+/**
+ * Funcao para imprimir display LCD quando o motor está parado
+ */
 void print_parado(){
   String string_velocidade = "";
+  String string_rpm = "";
   display.clearDisplay();   //Apaga o buffer e o display
   display.setCursor(0,0);
   display.println("PARADO");
   string_velocidade += vel;
   string_velocidade += "%";
   display.println(string_velocidade);
+  string_rpm += velocidade_RPM;
+  string_rpm += " RPM";
+  display.println(string_rpm);
   display.display();
 }
 
