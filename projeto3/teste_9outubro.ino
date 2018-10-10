f06bfd912cc643e5731db99c1ad7d1f07a84d8f7
MatheusFelix17 <42388283+MatheusFelix17@users.noreply.github.com>
2018-10-10 11:51:03 -0300

    Update: memória EEPROM funcionando

--- projeto3/teste_9outubro.ino
+++ projeto3/teste_9outubro.ino
@@ -19,21 +19,13 @@ void setup() {
 
   medeTemperatura();
   Serial.println(temperatura);
-  //Serial.println(temperatura);
-  writeEeprom(0, temperatura);
-  //Serial.println(readEeprom(0));
+  unsigned int temperaturaEscrita = (unsigned int)(temperatura * 10);
+  
+  Serial.println(temperaturaEscrita);
+//  writeEeprom(0, temperaturaEscrita);
+  Serial.print("Retorno da READ: ");
+  Serial.println(readEeprom(10));
 }
-
-
-//void loop() {
-////  Wire.beginTransmission(8); // transmit to device #8
-////  Wire.write("x is ");        // sends five bytes
-////  Wire.write(x);              // sends one byte
-////  Wire.endTransmission();    // stop transmitting
-////
-////  x++;
-////  delay(500);
-//}
 
 void loop() {
   
@@ -48,66 +40,80 @@ void writeEeprom(unsigned int memoryPosition, unsigned int temperatura){
   /*
    * Escrita do MS byte da temperatura
    */
-  //byte pageNumber = (byte)((memoryPosition & MS3B)>>8); // 3 bits of page adress
-  //byte i2cAddress = 0b01010000 | (byte)pageNumber; //set the I2C addresss
-  //Wire.beginTransmission(i2cAddress);
+  byte pageNumber = (byte)((memoryPosition & MS3B)>>8); // 3 bits of page adress
+  //Serial.println(pageNumber, BIN);
+  byte i2cAddress = 0b01010000 | (byte)pageNumber; //set the I2C addresss
+  //Serial.println(i2cAddress,BIN);
+  Wire.beginTransmission(i2cAddress);
 
-  //byte memAddress = (byte)(memoryPosition & LSB); //completando endereço de 11
-  //Wire.write(memAddress);
+  byte memAddress = (byte)(memoryPosition & LSB); //completando endereço de 11
+  //Serial.println(memAddress,BIN);
+  Wire.write(memAddress);
 
-  //byte msbTemp = (byte)((temperatura & MSB)>>8);  // tiramos os ultimos bits, e jogamos os mais significativos pra direita
-  //Wire.write(msbTemp);
-  byte dado = 0xF0;
-  int add = 1010000 | (memoryPosition >> 8) & 0x7;
-  Wire.write(add & 0xFF);
-  Wire.write(dado);
+  byte msbTemp = (byte)((temperatura & MSB)>>8);  // tiramos os ultimos bits, e jogamos os mais significativos pra direita
+  //Serial.print("MSB para escrita: ");
+  //Serial.println(msbTemp,BIN);
+  Wire.write(msbTemp);
+ 
 
-  Serial.print("End transmission 1: = ");
-  int c = Wire.endTransmission();
-  Serial.println(c);
-  
-  //Wire.endTransmission();
+  //Serial.print("End transmission 1: = ");
+  //int c = Wire.endTransmission();
+  //Serial.println(c);
+  Wire.endTransmission();
 
   delay(5);
 
   /*
    * Escrita do LS byte da temperatura
    */
-  //Wire.beginTransmission(i2cAddress);
+  Wire.beginTransmission(i2cAddress);
   //o MS byte da temperatura é salvo no endereço 0bxx...xx0 
   //o LS byte da temperatura é salvo no endereço 0bxx...xx1
   //assim podemos incrementar memAddress sem problemas
-  //memAddress += 1;
-  //Wire.write(memAddress);
+  memAddress += 1;
+  Wire.write(memAddress);
   
-  //byte lsbTemp = (byte)(temperatura & LSB); // tem que mascarar com 0xFF
-  //Wire.write(lsbTemp);
+  byte lsbTemp = (byte)(temperatura & LSB); // tem que mascarar com 0xFF
+  //Serial.print("LSB para escrita: ");
+  //Serial.println(lsbTemp,BIN);
+  Wire.write(lsbTemp);
 
   //Serial.print("End transmission 2: = ");
-  //c = Wire.endTransmission();
-  //Serial.println(c);
-  
-  //delay(5);
+//  c = Wire.endTransmission();
+//  Serial.println(c);
+
+  Wire.endTransmission();
+  delay(5);
 }
 
-byte readEeprom(unsigned int memPos) {
-  byte valor = 0x00;
+unsigned int readEeprom(unsigned int memPos) {
+  unsigned int valor = 0;
+  unsigned int valorLSB = 0;
+  unsigned int valorMSB = 0;
+  
   byte pageNumber = (byte)((memPos & MS3B)>>8); // 3 bits of page adress
   byte i2cAddress = 0b01010000 | (byte)pageNumber; //set the I2C addresss
   Wire.beginTransmission(i2cAddress);
   
   byte memAddress = (byte)(memPos & LSB); //completando endereço de 11
   Wire.write(memAddress);
-  memAddress += 1;
-  Wire.write(memAddress);
-  
-  Wire.requestFrom(i2cAddress, 1);
-  if (Wire.available()) {
-    Serial.println("Wire ta available");
-    valor = Wire.read();
-  }  
-  
   Wire.endTransmission();
+  Wire.requestFrom(i2cAddress, 2);
+  if (Wire.available()) {
+    valorMSB = Wire.read();
+    valorLSB = Wire.read();
+  }  
+
+//  Serial.print("valor MSB: ");
+//  Serial.println(valorMSB,BIN);
+//  Serial.print("valor LSB: ");
+//  Serial.println(valorLSB,BIN);
+
+  valor = valor | (valorMSB <<8);
+  valor = valor | valorLSB;
+
+//  Serial.print("valor na memoria: ");
+//  Serial.println(valor);
   
   return valor;
 }
