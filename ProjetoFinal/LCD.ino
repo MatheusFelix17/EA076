7c330d8f8fc7e7dc02f1f7fd377988c103f9ca01
MatheusFelix17 <42388283+MatheusFelix17@users.noreply.github.com>
2018-11-13 22:59:25 -0200

    Atualizado o LCD.ino, com as mensagens de voto

--- ProjetoFinal/LCD.ino
+++ ProjetoFinal/LCD.ino
@@ -1,5 +1,5 @@
 void display_print(int i) {
-  float temp = temperatura;
+
   switch(i) {  
     //menu principal
     case 0:
@@ -14,19 +14,133 @@ void display_print(int i) {
       display.println("Para cancelar ou confirmar");
       display.display();
     break;
-  
-    case 1:
+
+    case 11:
       display.clearDisplay();
       display.setCursor(0,0);
-      display.println("Tem Certeza?");
+      display.println("Numero: 11");
       display.setCursor(0,8);
-      display.println("* para cancelar");
+      display.println("Nome: Tony Stark");
       display.setCursor(0,16);
-      display.println("# para confirmar");
+      display.println("Partido: PSI");
+      display.setCursor(0,24);
+      display.println("Pressione # para confirmar");
+      display.setCursor(0,32);
+      display.println("Pressione * para cancelar");
       display.display();
     break;
-  
+
+    case 17:
+      display.clearDisplay();
+      display.setCursor(0,0);
+      display.println("Numero: 17");
+      display.setCursor(0,8);
+      display.println("Nome: Gustavo Simonini");
+      display.setCursor(0,16);
+      display.println("Partido: NSL");
+      display.setCursor(0,24);
+      display.println("Pressione # para confirmar");
+      display.setCursor(0,32);
+      display.println("Pressione * para cancelar");
+      display.display();
+    break;
+
+    case 22:
+      display.clearDisplay();
+      display.setCursor(0,0);
+      display.println("Numero: 22");
+      display.setCursor(0,8);
+      display.println("Nome: Bruce Wayne");
+      display.setCursor(0,16);
+      display.println("Partido: PCG");
+      display.setCursor(0,24);
+      display.println("Pressione # para confirmar");
+      display.setCursor(0,32);
+      display.println("Pressione * para cancelar");
+      display.display();
+    break;
+
+    case 33:
+      display.clearDisplay();
+      display.setCursor(0,0);
+      display.println("Numero: 33");
+      display.setCursor(0,8);
+      display.println("Nome: ");
+      display.setCursor(0,16);
+      display.println("Partido: ");
+      display.setCursor(0,24);
+      display.println("Pressione # para confirmar");
+      display.setCursor(0,32);
+      display.println("Pressione * para cancelar");
+      display.display();
+    break;
+
+    case 44:
+      display.clearDisplay();
+      display.setCursor(0,0);
+      display.println("Numero: 44");
+      display.setCursor(0,8);
+      display.println("Nome: ");
+      display.setCursor(0,16);
+      display.println("Partido: ");
+      display.setCursor(0,24);
+      display.println("Pressione # para confirmar");
+      display.setCursor(0,32);
+      display.println("Pressione * para cancelar");
+      display.display();
+    break;
+
+    case 55:
+      display.clearDisplay();
+      display.setCursor(0,0);
+      display.println("Numero: 55");
+      display.setCursor(0,8);
+      display.println("Nome: ");
+      display.setCursor(0,16);
+      display.println("Partido: ");
+      display.setCursor(0,24);
+      display.println("Pressione # para confirmar");
+      display.setCursor(0,32);
+      display.println("Pressione * para cancelar");
+      display.display();
+    break;
+
+    case 66:
+      display.clearDisplay();
+      display.setCursor(0,0);
+      display.println("Numero: 66");
+      display.setCursor(0,8);
+      display.println("Nome: Sheev Palpatine");
+      display.setCursor(0,16);
+      display.println("Partido: EMP");
+      display.setCursor(0,24);
+      display.println("Pressione # para confirmar");
+      display.setCursor(0,32);
+      display.println("Pressione * para cancelar");
+      display.display();
+    break;
+
+    case 100:
+      display.clearDisplay();
+      display.setCursor(0,0);
+      display.println("Voto registrado com sucesso!");
+      display.display();
+    break;
+    
+    //elses: voto NULO
     default:
+      display.clearDisplay();
+      display.setCursor(0,0);
+      display.println(i);
+      display.setCursor(0,8);
+      display.println("NUMERO ERRADO");
+      display.setCursor(4,16);    //tentar centralizar, nao sei se vai bugar igual quando aumentamos text size
+      display.println("VOTO NULO");
+      display.setCursor(0,24);
+      display.println("Pressione # para confirmar");
+      display.setCursor(0,32);
+      display.println("Pressione * para cancelar");
+      display.display();
     break;
 
   }
@@ -40,6 +154,4 @@ int memoriaLivre(unsigned int memPosition){
   }
   return espacoLivre;
 }
-
-
 
